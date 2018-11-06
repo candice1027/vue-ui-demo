dd3011a47c79cae72162b72406a1637d8c6b9e25
lvsuzhen <1205521940@qq.com>
2018-11-06 20:32:40 +0800

    create a button commponent

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 一个vue UI 组件
 #### 作者： lvsz
+
+## 使用本框架前，请在css中开启border-box
+```
+*{box-sizing: border-box}
+```
---
+++ button.js
@@ -0,0 +1,9 @@
+Vue.component('g-button',{
+    data: function() {
+        return {
+            name: 222
+        }
+    },
+    template: '<button>这里是g-buttom</button>'
+
+})
