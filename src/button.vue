d1c4d5ca196b47a5fbeaa4896ecf6f0a67716d78
lvsuzhen <1205521940@qq.com>
2018-11-07 21:41:02 +0800

    add icon

--- src/button.vue
+++ src/button.vue
@@ -1,11 +1,18 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+
+    </button>
 </template>
 <script>
     export default {
-
+        props: ['icon','iconPosition']
     }
-
 </script>
 <style lang="scss">
     .g-button {
@@ -16,9 +23,12 @@
         border: 1px solid var(--border-color);
         padding: 0 1em;
         border-radius: var(--border-radius);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
         &:hover {
             border-color: var(--border-color-hover);
-
         }
         &:active {
             background: var(--button-active-bg);
@@ -26,7 +36,23 @@
         &:focus {
             outline: none;
         }
+        > .icon {
+            order: 1;
+            margin-right: .3em;
+        }
+        > .content {
+            order: 2;
+        }
+        &.icon-right {
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left: .3em;
+            }
+            > .content {
+                order: 1;
+            }
+
+        }
     }
-
-
 </style>
