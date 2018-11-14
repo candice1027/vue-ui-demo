04b3dc8d2244589acacac97d0fe1c96c389a9e38
lvsuzhen <1205521940@qq.com>
2018-11-14 21:47:54 +0800

    input 增加样式

--- src/input.vue
+++ src/input.vue
@@ -1,16 +1,36 @@
 <template>
-    <div class="wrapper">
-        <input type="text" :value="value">
-    </div>
+    <div class="wrapper" :class="{'error': error}">
+        <input type="text" :value="value" :disabled="disabled" :readonly="readonly">
+        <template v-if="error">
+            <g-icon icon="err"></g-icon>
+            <span class="errorMessage">{{error}}</span>
+        </template>
 
+    </div>
 </template>
 <script>
+    import Icon from './icon';
     export default {
         props: {
             value: {
                 type: String
+            },
+            disabled:{
+                type: Boolean,
+                default: false
+            },
+            readonly: {
+                type: Boolean,
+                default: false
+            },
+            error: {
+                type: String,
             }
+        },
+        components: {
+            'g-icon': Icon
         }
+
 
     }
 </script>
@@ -21,9 +41,15 @@
     $border-radius: 4px;
     $font-size: 12px;
     $box-shadow-color: rgba(0,0,0,.5);
+    $red: #F1453D;
     .wrapper {
         font-size: $font-size;
-        display: inline-block;
+        display: inline-flex;
+        align-items: center;
+        * {
+            margin-right: .5em;
+            &:last-child{margin-right: 0;}
+        }
         > input {
             height: 32px;
             border: 1px solid $border-color;
@@ -37,6 +63,17 @@
                 box-shadow: $box-shadow-color;
                 outline: none;
             }
+            &[disabled],&[readonly] {
+                border-color: #aaa;
+                color: #aaa;
+                cursor: not-allowed;
+            }
+        }
+        &.error > input {
+            border-color:$red;
+        }
+        .errorMessage {
+            color: $red;
         }
     }
 
